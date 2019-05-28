5b65a288101cf2bb5684c4efcc75296fba95e819
Sanushen <sanushen@gmail.com>
2019-05-28 16:36:43 +0200

    Remove ignored files

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/home/sanu/Sites/dummy-symfony-for-copying-files/node_modules/gatsby-plugin-transition-link/gatsby-browser.js'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('/home/sanu/Sites/dummy-symfony-for-copying-files/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
